27b43687615329bc07b9dff66165cc44f78bc11d
Indranil-Sarmah <isharma055@gmail.com>
2021-02-10 21:23:53 +0530

    added virtual methods and encrytiop technique for hashing the password

--- GoodReadsBackend/models/user.js
+++ GoodReadsBackend/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-const crypto = require("crypto");
-const uuidv1 = require("uuid/v1");
+const crypto = require("crypto");//core node.js module to encrypt the password
+const uuidv1 = require("uuid/v1");//unique strings for userId 
 
+//to create a schema in mongoDB
 const userSchema = new mongoose.Schema(
     {
         name: {
@@ -14,7 +15,7 @@ const userSchema = new mongoose.Schema(
             type: String,
             trim: true,
             required: true,
-            unique: 32
+            unique: true
         },
         hashed_password: {
             type: String,
@@ -36,3 +37,34 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true }
 );
+
+// virtual field
+userSchema
+    .virtual("password")
+    .set(function(password) {
+        this._password = password;
+        this.salt = uuidv1();
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
+
+    // add methods in mongoose to perform some action
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if (!password) return "";
+        try {
+            //crypto has this inbuilt function,refer nodejs documentation 
+            return crypto
+                .createHmac("sha1", this.salt)//salt is a long complicated unique string
+                .update(password)
+                .digest("hex");
+        } catch (err) {
+            return "";
+        }
+    }
+};
+
+//finally exporting the schema by creating the model using model method of mongoose
+module.exports = mongoose.model("User", userSchema);
