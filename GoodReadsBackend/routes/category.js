727517770597a954415680f54aeef516dcc26925
Indranil-Sarmah <isharma055@gmail.com>
2021-02-16 09:44:21 +0530

    category update delete view

--- GoodReadsBackend/routes/category.js
+++ GoodReadsBackend/routes/category.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/category");
+const { create,categoryById, read,update,remove,list} = require("../controllers/category");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
+router.get("/category/:categoryId", read);
 router.post("/category/create/:userId",requireSignin, isAuth, isAdmin, create);
+router.put(
+    "/category/:categoryId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    update
+);
+router.delete(
+    "/category/:categoryId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    remove
+);
+router.get("/categories", list);
 //anytime there is userId in the routes this method will run 
+
+router.param("categoryId", categoryById);
 router.param("userId", userById);
 
 module.exports = router;
